1ce311d0dd0beaef11b384710c9556db7c224236
Prakash <Bhavya.Prakash@bain.com>
2026-04-15 13:33:08 +0530

    deployment

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_openai import OpenAI
+# from langchain_openai import OpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
